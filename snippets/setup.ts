4f8d25f0d891f6aa8a11bd06f56eb5dacc2a97ca
jxom <7336481+jxom@users.noreply.github.com>
2025-10-07 08:10:58 +1100

    refactor: polish structure + hygene (#22)
    * chore: add biome
    
    * u
    
    * refactor: restructure
    
    * chore: up
    
    * chore: install biome
    
    * chore: up
    
    * chore: up
    
    * chore: rm redundant dep
    
    * Update package.json
    
    * chore: pnpm
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    ---------
    
    Co-authored-by: Zygimantas <5236121+Zygimantass@users.noreply.github.com>

---
+++ snippets/setup.ts
@@ -0,0 +1,22 @@
+// @ts-nocheck
+// [!region setup]
+import { tempoLento } from 'tempo/chains'
+import { createTempoClient, tempoActions } from 'tempo/viem'
+import { http, publicActions, walletActions } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+
+export const client = createTempoClient({
+  account: privateKeyToAccount('0x...'),
+  chain: tempoLento,
+  transport: http(undefined, {
+    fetchOptions: {
+      headers: {
+        Authorization: `Basic ${btoa(`${process.env['TEMPO_RPC_USER']}:${process.env['TEMPO_RPC_PASSWORD']}`)}`,
+      },
+    },
+  }),
+})
+  .extend(publicActions)
+  .extend(walletActions)
+  .extend(tempoActions())
+// [!endregion setup]
