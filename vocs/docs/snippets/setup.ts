4d89f8863e2910bb5f7b45cf85b1d51158618f38
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2025-10-06 21:53:47 +0200

    tempo lento

--- vocs/docs/snippets/setup.ts
+++ vocs/docs/snippets/setup.ts
@@ -1,12 +1,12 @@
 // [!region setup]
-import { createClient, http, publicActions, walletActions } from 'viem'
-import { tempo } from 'tempo/chains';
-import { tempoActions } from 'tempo/viem';
+import { http, publicActions, walletActions } from 'viem'
+import { tempoLento } from 'tempo/chains';
+import { createTempoClient, tempoActions } from 'tempo/viem';
 import { privateKeyToAccount } from 'viem/accounts';
 
-export const client = createClient({
+export const client = createTempoClient({
   account: privateKeyToAccount('0x...'),
-  chain: tempo,
+  chain: tempoLento,
   transport: http(undefined, {
     fetchOptions: {
       headers: {
@@ -17,5 +17,4 @@ export const client = createClient({
 })
   .extend(publicActions)
   .extend(walletActions)
-  .extend(tempoActions());
 // [!endregion setup]
