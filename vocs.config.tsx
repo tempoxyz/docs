23b9f603854f075849924c41073f160e177b851c
Liam Horne <liam@lihorne.com>
2025-11-17 09:14:48 -0500

    docs: proper noun quick start -> quickstart

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -51,7 +51,7 @@ export default defineConfig({
   sidebar: {
     '/': [
       {
-        text: 'Quick Start',
+        text: 'Quickstart',
         items: [
           {
             text: 'Overview',
