d208320022955020e099b472787932eda935d4df
Derek <256792747+decofe@users.noreply.github.com>
2026-06-26 19:26:32 +0000

    Proxy developers paths through docs

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
+    base: '/developers',
     plugins: [
       blogPostsPlugin(),
       marketingSearchIndexPlugin({ source: 'vocs' }),
