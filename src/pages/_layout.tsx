1e99ffc6c8fdf001fc78a8a31892f4f58b11f912
Derek Cofausper <256792747+decofe@users.noreply.github.com>
2026-06-28 06:15:26 +0300

    Remove redundant RSC fetch monkey patches (#603)

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,36 +1,5 @@
 import type { PropsWithChildren } from 'react'
 
-export const normalizeProxiedRscFetch = `
-(() => {
-  if (window.__tempoNormalizeProxiedRscFetch) return;
-  window.__tempoNormalizeProxiedRscFetch = true;
-  const originalFetch = window.fetch.bind(window);
-  window.fetch = (input, init) => {
-    const url = typeof input === 'string'
-      ? input
-      : input instanceof URL
-        ? input.toString()
-        : input.url;
-    const requestUrl = new URL(url, window.location.href);
-    let rewritten = url;
-    if (requestUrl.pathname.startsWith('/RSC/R/')) {
-      const pathname = requestUrl.pathname
-        .replace(/\\/RSC\\/R\\/developers\\.txt$/, '/RSC/R/_root.txt')
-        .replace(/\\/RSC\\/R\\/developers\\//, '/RSC/R/');
-      rewritten = new URL(
-        pathname + requestUrl.search + requestUrl.hash,
-        window.location.origin,
-      ).toString();
-    }
-
-    if (rewritten === url) return originalFetch(input, init);
-    if (typeof input === 'string' || input instanceof URL) return originalFetch(rewritten, init);
-
-    return originalFetch(new Request(rewritten, input), init);
-  };
-})();
-`
-
 export default function Layout(
   props: PropsWithChildren<{
     path: string
@@ -46,8 +15,6 @@ export default function Layout(
         type="font/woff2"
         crossOrigin="anonymous"
       />
-      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: static bootstrap must run before the RSC client bundle. */}
-      <script dangerouslySetInnerHTML={{ __html: normalizeProxiedRscFetch }} />
       {props.children}
     </>
   )
