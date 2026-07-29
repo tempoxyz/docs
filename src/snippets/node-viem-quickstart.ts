424824b4d826dff7123db57bb33551de8e9a8540
Parv Ahuja <17094219+parvahuja@users.noreply.github.com>
2026-07-28 18:44:50 -0700

    docs: simplify Node Viem quickstart

--- src/snippets/node-viem-quickstart.ts
+++ src/snippets/node-viem-quickstart.ts
@@ -1,11 +1,8 @@
 // [!region testnet]
-import { env } from 'node:process'
 import { Account, createClient } from 'viem/tempo'
 
-const privateKey = env.TEMPO_TEST_PRIVATE_KEY
-if (!privateKey) throw new Error('Set TEMPO_TEST_PRIVATE_KEY')
-
-const account = Account.fromSecp256k1(privateKey as `0x${string}`)
+const privateKey = '0x...'
+const account = Account.fromSecp256k1(privateKey)
 const client = createClient({ account, testnet: true })
 
 await client.faucet.fundSync({ account })
